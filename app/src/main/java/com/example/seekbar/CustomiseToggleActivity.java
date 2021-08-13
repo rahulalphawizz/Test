189c251472a4a817af0a27e1b8c3276e179083ed
rahulalphawizz <rahulalphawizz@gmail.com>
2021-08-13 14:17:56 +0530

    commit 13/aug

---
+++ app/src/main/java/com/example/seekbar/CustomiseToggleActivity.java
@@ -0,0 +1,69 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class CustomiseToggleActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tv_demo_notiterested,tv_demo_interested;
+    ImageView imgAddnDetailsArrow;
+
+    LinearLayout ll_partner_services;
+    boolean isExpand = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customise_toggle);
+
+        tv_demo_interested = findViewById(R.id.tv_demo_interested);
+        tv_demo_notiterested = findViewById(R.id.tv_demo_notiterested);
+        imgAddnDetailsArrow = findViewById(R.id.imgAddnDetailsArrow);
+        ll_partner_services = findViewById(R.id.ll_partner_services);
+
+        imgAddnDetailsArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               ll_partner_services.setVisibility(isExpand ? (View.VISIBLE) : View.GONE);
+               isExpand  = !isExpand;//?false:true;
+                //isExpand = "no"? ll_partner_services.setVisibility(View.VISIBLE);
+                //isExpand = ll_partner_services.setVisibility(View.GONE);
+
+                //ll_partner_services.setVisibility(View.VISIBLE);
+            }
+        });
+
+        tv_demo_interested.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_demo_interested.setBackgroundResource(R.drawable.background_selected_toggle);
+                tv_demo_notiterested.setBackground(null);
+            }
+        });
+
+        tv_demo_notiterested.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_demo_interested.setBackground(null);
+                tv_demo_notiterested.setBackgroundResource(R.drawable.background_selected_toggle);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.tv_csm_interested:
+
+
+
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/seekbar/Palindrom.java
@@ -0,0 +1,24 @@
+package com.example.seekbar;
+
+import android.util.Log;
+
+    class Palindrom{
+        public static void main(String args[]){
+
+
+            System.out.println("aaaaaaaaaaaa");
+            int r,sum=0,temp;
+            int n=454;//It is the number variable to be checked for palindrome
+
+            temp=n;
+            while(n>0){
+                r=n%10;  //getting remainder
+                sum=(sum*10)+r;
+                n=n/10;
+            }
+            if(temp==sum)
+                System.out.println("palindrome number ");
+            else
+                System.out.println("not palindrome");
+        }
+    }
