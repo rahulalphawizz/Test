87a949e9d6d63125d24e8bedab55a25fc615eec5
rahulalphawizz <rahulalphawizz@gmail.com>
2022-03-03 16:27:44 +0530

    customise

--- app/src/main/java/com/example/seekbar/CustomiseToggleActivity.java
+++ app/src/main/java/com/example/seekbar/CustomiseToggleActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CustomiseToggleActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,14 +16,18 @@ public class CustomiseToggleActivity extends AppCompatActivity implements View.O
     ImageView imgAddnDetailsArrow;
 
     LinearLayout ll_partner_services;
+
+    ImageView imageView;
+    Button button;
+    RelativeLayout relativeLayout;
     boolean isExpand = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customise_togglenew);
+        setContentView(R.layout.activity_customise_toggle);
 
-        //tv_demo_interested = findViewById(R.id.tv_demo_interested);
-       // tv_demo_notiterested = findViewById(R.id.tv_demo_notiterested);
+        tv_demo_interested = findViewById(R.id.tv_demo_interested);
+        tv_demo_notiterested = findViewById(R.id.tv_demo_notiterested);
         imgAddnDetailsArrow = findViewById(R.id.imgAddnDetailsArrow);
         ll_partner_services = findViewById(R.id.ll_partner_services);
 
@@ -38,7 +44,7 @@ public class CustomiseToggleActivity extends AppCompatActivity implements View.O
             }
         });
 
-        /*tv_demo_interested.setOnClickListener(new View.OnClickListener() {
+        tv_demo_interested.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv_demo_interested.setBackgroundResource(R.drawable.background_selected_toggle);
@@ -51,8 +57,8 @@ public class CustomiseToggleActivity extends AppCompatActivity implements View.O
             public void onClick(View v) {
                 tv_demo_interested.setBackground(null);
                 tv_demo_notiterested.setBackgroundResource(R.drawable.background_selected_toggle);
-            }*/
-        //});
+            }
+        });
     }
 
     @Override
